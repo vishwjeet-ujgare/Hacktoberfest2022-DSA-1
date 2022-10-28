b6f2f2481a06c1008cc50b67959aa66da59c05f4
vishwjeet-ujgare <105901362+vishwjeet-ujgare@users.noreply.github.com>
2022-10-28 14:51:17 +0530

    reverse string update the code and optimize it
    used StringBuffer here

--- Java/reverseString.java
+++ Java/reverseString.java
@@ -1,14 +1,16 @@
-class reverseString { 
-	public static void main (String[] args) { 
-		String str= "Saksham", nstr=""; 
-		char ch; 
-		
-	System.out.print("Original word: Saksham");
-	for (int i=0; i<str.length(); i++) 
-	{ 
-		ch= str.charAt(i);
-		nstr= ch+nstr;
-	} 
-	System.out.println("Reversed word: "+ nstr); 
-	} 
-} 
+ import java.lang.*;
+import java.io.*;
+import java.util.*;
+  
+public class reverseString {
+    public static void main(String[] args)
+    {
+        String str = "Saksham";
+  
+        // conversion from String object to StringBuffer
+        StringBuffer sbr = new StringBuffer(str);
+        // To reverse the string
+        sbr.reverse();
+        System.out.println(sbr);
+    }
+}
